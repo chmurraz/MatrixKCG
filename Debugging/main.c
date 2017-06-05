fc9a5080273b5a02c3c5ca02567805c83679ca74
Chris <chris.murray.az@gmail.com>
2017-06-05 16:52:33 -0700

    Update

--- Debugging/main.c
+++ Debugging/main.c
@@ -7,6 +7,7 @@ int main()
 	array_float32_3_3 A;
 	array_float32_3_3 ARREF;
 	array_float32_3 b;
+	array_float32_3 bRREF;
 	array_float32_3 x;
 	kcg_int32 n = 3;
 	kcg_int32 rank = 0;
@@ -29,7 +30,6 @@ int main()
 	//	Solution will be -1/5, 4, -4/5
 	*/
 
-	/*
 	//	Case 2
 	A[0][0] = 2.0;
 	A[0][1] = 4.0;
@@ -44,8 +44,8 @@ int main()
 	b[1] = 3.0;
 	b[2] = -10.0;
 	//	Solution will be 4, 3, true
-	*/
 
+	/*
 	//	Case 3
 	A[0][0] = 5.0;
 	A[0][1] = -2.0;
@@ -59,18 +59,20 @@ int main()
 	b[0] = 2.0;
 	b[1] = 10.0;
 	b[2] = -4.0;
+	*/
 
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
 			ARREF[i][j] = A[i][j];
+		bRREF[i] = b[i];
 	}
 
 	x[0] = 0.0;
 	x[1] = 0.0;
 	x[2] = 0.0;
 
-	Gauss_3(&A, &ARREF, &b, n, &x, &rank, &consistent);
+	Gauss_3(&A, &ARREF, &b, &bRREF, n, &x, &rank, &consistent);
 
 	
 
